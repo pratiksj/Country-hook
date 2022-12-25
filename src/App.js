23a6d610c7618d5ebeaf54d5a87d37a2e3403a52
pratiksj <pratiksharai728@gmail.com>
2022-12-25 10:58:41 +0545

    feat:7.7 used api endpoint full name to fetch  country's details

--- src/App.js
+++ src/App.js
@@ -14,23 +14,45 @@ const useField = (type) => {
     onChange,
   };
 };
-
 const useCountry = (name) => {
   const [country, setCountry] = useState(null);
   useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then((result) => {
-      const filteredCountry = result.data.find(
-        (state) => state.name.common.toLowerCase() === name.toLowerCase()
-      );
-      // if (filteredCountry.length === 1) {
-      setCountry(filteredCountry);
-      // }
-      // return country;
-    });
+    axios
+      .get(`https://restcountries.com/v3.1/name/${name}`)
+      .then((result) => {
+        setCountry(...result.data);
+      })
+      .catch((error) => setCountry(null));
+
+    // axios.get("https://restcountries.com/v3.1/all").then((result) => {
+    //   const filteredCountry = result.data.find(
+    //     (state) => state.name.common.toLowerCase() === name.toLowerCase()
+    //   );
+    //   // if (filteredCountry.length === 1) {
+    //   setCountry(filteredCountry);
+    //   // }
+    //   // return country;
+    // });
   }, [name]);
 
   return country;
 };
+
+// const useCountry = (name) => {
+//   const [country, setCountry] = useState(null);
+//   useEffect(() => {
+//     const getData = async () => {
+//       const response = await axios.get("https://restcountries.com/v3.1/all");
+//       const filteredCountry = response.data.find(
+//         (state) => state.name.common.toLowerCase() === name.toLowerCase()
+//       );
+//       setCountry(filteredCountry);
+//     };
+//     getData();
+//   }, [name]);
+
+//   return country;
+// };
 
 const Country = ({ country }) => {
   console.log("insane", country);
@@ -75,8 +97,8 @@ const App = () => {
         <input {...nameInput} />
         <button>find</button>
       </form>
-
-      <Country country={country} />
+      {!name ? null : <Country country={country} />}
+      {/* <Country country={country} /> */}
     </div>
   );
 };
